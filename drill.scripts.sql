c37909b75cb874ab01eb349e5921a34e8d5e5a9d
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2020-09-28 11:17:58 -0500

    added assignment drills 3-4

--- drill.scripts.sql
+++ drill.scripts.sql
@@ -17,3 +17,39 @@ on p2.id = ep.project_id
 where project_name = 'Plan christmas party';
 
 -- List the names of employees from the Warehouse department that are assigned to the 'Watch paint dry' project.
+
+select emp_name, project_name, dept_name
+from employee e2
+join employee_project ep 
+on ep.emp_id = e2.id
+join project p2 
+on p2.id = ep.project_id
+join department d 
+on d.id = e2.department
+where project_name = 'Watch paint dry'
+and dept_name = 'Warehouse';
+
+-- Which projects are the Sales department employees assigned to?
+
+select emp_name, project_name, dept_name
+from employee e2
+join employee_project ep 
+on ep.emp_id = e2.id
+join project p2 
+on p2.id = ep.project_id
+join department d 
+on d.id = e2.department
+where dept_name = 'Sales';
+
+-- List only the managers that are assigned to the 'Watch paint dry' project.
+
+select emp_name, project_name, dept_name
+from employee e2
+join employee_project ep 
+on ep.emp_id = e2.id
+join project p2 
+on p2.id = ep.project_id
+join department d 
+on d.id = e2.department
+where dept_name = 'Sales'
+and d.manager = e2.id;
